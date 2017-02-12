53dcba81cdb58091001c551c101bbbb2aab024b8
Ander González <ander.gonzalezt@alumni.mondragon.edu>
2017-02-12 13:56:58 +0100

    Comment Fixes + NaCl symetric class
    Signed-off-by: Ander González <ander.gonzalezt@alumni.mondragon.edu>

--- step1.py
+++ step1.py
@@ -3,4 +3,4 @@ import muCrypto
 if __name__ == '__main__':
     sym_cripter = muCrypto.SymetricCrypterKcz()
     sym_cripter.decrypt(r'keys_and_files/texto_cifrado_paso2_keyczar.base64',
-                        r'keys_and_files/texto_descifrado_paso2_keyczar.base64')
+                        r'keys_and_files/texto_descifrado_paso2_keyczar.base64', input_encoding=None)
